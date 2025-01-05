604fcb012b7b563cd4322d0fd7d8b4b81921283a
MAOFU <2148334528@qq.com>
2025-01-05 23:50:22 +0800

    feat: 初始化

---
+++ .eslintrc.tmp.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "root": true,
+    "extends": [
+        "eslint-config-ay",
+        "eslint-config-ay/import",
+        "eslint-config-ay/nodejs",
+        "eslint-config-ay/typescript"
+    ]
+}
