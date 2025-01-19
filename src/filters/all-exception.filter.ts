18fdd1c016702be32a60b1d114ec77fa60a295ad
MAOFU <2148334528@qq.com>
2025-01-19 20:20:11 +0800

    feat: 实现 全局异常处理

---
+++ src/filters/all-exception.filter.ts
@@ -0,0 +1,40 @@
+import { ResCodeEnum } from '@/enum';
+import { getCommonRes } from '@/utils';
+import {
+    ExceptionFilter,
+    HttpException,
+    HttpStatus,
+    ArgumentsHost,
+    Catch
+} from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+import { QueryFailedError } from 'typeorm';
+
+/** 全局异常处理 */
+@Catch()
+export class AllExceptionFilter implements ExceptionFilter {
+    constructor (private readonly httpAdapterHost: HttpAdapterHost,) {}
+
+    /** */
+    catch (exception, host: ArgumentsHost) {
+        const { httpAdapter } = this.httpAdapterHost;
+        const ctx = host.switchToHttp();
+        // const request = ctx.getRequest();
+        const response = ctx.getResponse();
+
+        const httpStatus = exception instanceof HttpException ? exception.getStatus()  : HttpStatus.INTERNAL_SERVER_ERROR;
+
+        let msg = exception?.response?.message || 'Internal Server Error';
+
+        // 数据库查询异常，获取更加准确的msg
+        if (exception instanceof QueryFailedError) {
+            msg = exception.message;
+        }
+
+        const res = getCommonRes({ success: false, code: ResCodeEnum.fail, message: msg });
+
+        console.error(res);
+
+        httpAdapter.reply(response, res, httpStatus);
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ReqValidationPipe } from './pipes/validation.pipe';
+import { AllExceptionFilter } from './filters/all-exception.filter';
 
 /** */
 async function bootstrap () {
     const app = await NestFactory.create(AppModule);
+    const httpAdapter = app.get(HttpAdapterHost);
 
     // 全局管道
     app.useGlobalPipes(ReqValidationPipe);
+
+    // // 全局Filter只能有一个
+    app.useGlobalFilters(new AllExceptionFilter(httpAdapter));
 
     await app.listen(3005);
 }
