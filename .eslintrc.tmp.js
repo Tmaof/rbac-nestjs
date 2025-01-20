f43523ba01046064f0742e891973bf53b7b301a0
MAOFU <2148334528@qq.com>
2025-01-21 00:12:54 +0800

    feat: 删除无用文件 .eslintrc.tmp.js

--- .eslintrc.tmp.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    "root": true,
-    "extends": [
-        "eslint-config-ay",
-        "eslint-config-ay/import",
-        "eslint-config-ay/nodejs",
-        "eslint-config-ay/typescript"
-    ]
-}
